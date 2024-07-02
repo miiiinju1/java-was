fa477e835a5000af8658cbbdf0e3615b7273604b
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-02 12:28:07 +0900

    refactor: server 생성을 책임지는 ServerInitializer 클래스로 로직 이동

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -3,39 +3,21 @@ package codesquad;
 import codesquad.handler.ConnectionHandler;
 import codesquad.handler.ResourceHandler;
 import codesquad.http.HttpHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import codesquad.server.ServerInitializer;
 
 public class Main {
 
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        ConnectionHandler connectionHandler = new ConnectionHandler(new HttpHandler(), new ResourceHandler());
-        log.debug("Listening for connection on port 8080 ....");
+    public static void main(String[] args) {
+        ServerInitializer serverInitializer = new ServerInitializer();
+        HttpHandler httpHandler = new HttpHandler();
+        ResourceHandler resourceHandler = new ResourceHandler();
+        ConnectionHandler connectionHandler = new ConnectionHandler(httpHandler, resourceHandler);
 
-        while (true) {
-            try {
-                Socket clientSocket = serverSocket.accept();
-                executorService.execute(() -> {
-                    try (clientSocket) {
-                        connectionHandler.handleConnection(clientSocket);
-                    } catch (IOException e) {
-                        log.error("Failed to handle connection", e);
-                    }
-                });
-            }
-            catch (IOException e) {
-                log.error("Failed to accept connection", e);
-            }
+        try {
+            serverInitializer.startServer(8080, connectionHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
