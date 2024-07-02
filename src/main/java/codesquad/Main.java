ce398c32b659b2e9e7133b81b5b2f35fb80111c4
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-02 11:21:45 +0900

    feat: Main에 ExecutorService를 두고, 새로운 요청이 올 때마다 별도 스레드에서 serverSocket을 connectionHandler에 넘겨주고 close하도록 구현

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,70 +1,41 @@
 package codesquad;
 
+import codesquad.handler.ConnectionHandler;
 import codesquad.handler.ResourceHandler;
 import codesquad.http.HttpHandler;
-import codesquad.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
-        HttpHandler httpHandler = new HttpHandler();
-        ResourceHandler resourceHandler = new ResourceHandler();
+        ConnectionHandler connectionHandler = new ConnectionHandler(new HttpHandler(), new ResourceHandler());
         log.debug("Listening for connection on port 8080 ....");
 
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            try (Socket clientSocket = serverSocket.accept()) { // 클라이언트 연결을 수락합니다.
-                InputStream requestStream = clientSocket.getInputStream();
-                OutputStream responseStream = clientSocket.getOutputStream();
-
-                HttpRequest httpRequest = httpHandler.parseRequest(requestStream);
-
-                try {
-                    InputStream inputStream = resourceHandler.readFileAsStream(httpRequest.getPath());
-                    log.debug("request : {}", httpRequest);
-                    sendResponse(responseStream, inputStream);
-                }
-                catch (IllegalArgumentException e) {
-                    log.error("File not found! : {}", httpRequest.getPath());
-                    sendNotFoundResponse(responseStream);
-                }
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                executorService.execute(() -> {
+                    try (clientSocket) {
+                        connectionHandler.handleConnection(clientSocket);
+                    } catch (IOException e) {
+                        log.error("Failed to handle connection", e);
+                    }
+                });
+            }
+            catch (IOException e) {
+                log.error("Failed to accept connection", e);
             }
         }
-    }
-
-    private static void sendResponse(OutputStream outputStream, InputStream fileInputStream) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-        bw.write("HTTP/1.1 200 OK");
-        bw.newLine();
-        bw.write("Content-Type: text/html; charset=UTF-8");
-        bw.newLine();
-        bw.newLine();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            bw.write(line);
-            bw.newLine();
-        }
-        bw.flush();
-    }
-
-    private static void sendNotFoundResponse(OutputStream outputStream) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
-        bw.write("HTTP/1.1 404 Not Found");
-        bw.newLine();
-        bw.write("Content-Type: text/html; charset=UTF-8");
-        bw.newLine();
-        bw.newLine();
-        bw.write("<html><body><h1>404 Not Found</h1></body></html>");
-        bw.flush();
     }
 }
---
+++ src/main/java/codesquad/handler/ConnectionHandler.java
@@ -0,0 +1,67 @@
+package codesquad.handler;
+
+import codesquad.http.HttpHandler;
+import codesquad.http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ConnectionHandler {
+
+    private final HttpHandler httpHandler;
+    private final ResourceHandler resourceHandler;
+    private static final Logger log = LoggerFactory.getLogger(ConnectionHandler.class);
+
+    public ConnectionHandler(HttpHandler httpHandler, ResourceHandler resourceHandler) {
+        this.httpHandler = httpHandler;
+        this.resourceHandler = resourceHandler;
+    }
+
+    public void handleConnection(final Socket clientSocket) throws IOException {
+        final InputStream requestStream = clientSocket.getInputStream();
+        final OutputStream responseStream = clientSocket.getOutputStream();
+
+        final HttpRequest httpRequest = httpHandler.parseRequest(requestStream);
+
+        try {
+            final InputStream inputStream = resourceHandler.readFileAsStream(httpRequest.getPath());
+            log.debug("request : {}", httpRequest);
+            sendResponse(responseStream, inputStream);
+        }
+        catch (IllegalArgumentException e) {
+            log.error("File not found! : {}", httpRequest.getPath());
+            sendNotFoundResponse(responseStream);
+        }
+
+    }
+
+    private void sendResponse(OutputStream outputStream, InputStream fileInputStream) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+        bw.write("HTTP/1.1 200 OK");
+        bw.newLine();
+        bw.write("Content-Type: text/html; charset=UTF-8");
+        bw.newLine();
+        bw.newLine();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            bw.write(line);
+            bw.newLine();
+        }
+        bw.flush();
+    }
+
+    private void sendNotFoundResponse(OutputStream outputStream) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+        bw.write("HTTP/1.1 404 Not Found");
+        bw.newLine();
+        bw.write("Content-Type: text/html; charset=UTF-8");
+        bw.newLine();
+        bw.newLine();
+        bw.write("<html><body><h1>404 Not Found</h1></body></html>");
+        bw.flush();
+    }
+}
