b7142a74856deca40d749b2a60c359f4cbcbbf51
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-02 14:41:38 +0900

    refactor: HttpHeaders 내 필드 변경, 메서드 오타 수정

--- src/main/java/codesquad/http/HttpHeaders.java
+++ src/main/java/codesquad/http/HttpHeaders.java
@@ -6,29 +6,29 @@ import java.util.Set;
 
 public class HttpHeaders {
 
-    private final Map<String, String> headerMap;
+    private final Map<String, String> valueMap;
 
     public static HttpHeaders of(Map<String, String> headers) {
         return new HttpHeaders(headers);
     }
 
     public void addHeader(String key, String value) {
-        headerMap.put(key, value);
+        valueMap.put(key, value);
     }
 
     public String getHeader(String key) {
-        String value = headerMap.get(key);
+        String value = valueMap.get(key);
         if(value == null) {
             throw new IllegalArgumentException("Header not found : " + key);
         }
         return value;
     }
 
-    public Set<Map.Entry<String, String>> gegHeaders() {
-        return headerMap.entrySet();
+    public Set<Map.Entry<String, String>> getValues() {
+        return valueMap.entrySet();
     }
 
-    private HttpHeaders(Map<String, String> headerMap) {
-        this.headerMap = new HashMap<>(headerMap);
+    private HttpHeaders(Map<String, String> valueMap) {
+        this.valueMap = new HashMap<>(valueMap);
     }
 }
