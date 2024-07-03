84a671e9235eb49f13eb7a2040d9a565f642b97b
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-03 12:25:55 +0900

    chore: ResourceHandler에서 사용하지 않는 메서드 제거

--- src/main/java/codesquad/handler/ResourceHandler.java
+++ src/main/java/codesquad/handler/ResourceHandler.java
@@ -14,18 +14,6 @@ public class ResourceHandler implements HttpHandler {
     private static final String STATIC_PATH = "static";
     private static final Logger log = LoggerFactory.getLogger(ResourceHandler.class);
 
-    public InputStream readFileAsStream(String filePath) {
-        ClassLoader classLoader = ResourceHandler.class.getClassLoader();
-
-        InputStream inputStream = classLoader.getResourceAsStream(STATIC_PATH + filePath);
-
-        if (inputStream == null) {
-            throw new IllegalArgumentException("File not found! : static/" + filePath);
-        }
-
-        return inputStream;
-    }
-
     @Override
     public void handle(HttpRequest request, HttpResponse response) throws Exception {
         final String filePath = request.getPath().getValue();
