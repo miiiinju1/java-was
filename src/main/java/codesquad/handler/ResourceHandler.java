d429a0481694be73087e1204999a41347be6b0bb
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-04 09:57:37 +0900

    feat: 대상 path가 디렉터리일 경우 /index.html을 찾아서 반환하도록 수정하고, 파일 확장자가 있는 경우 파일을 반환하도록 구현

--- src/main/java/codesquad/handler/ResourceHandler.java
+++ src/main/java/codesquad/handler/ResourceHandler.java
@@ -7,6 +7,7 @@ import codesquad.http.header.HeaderConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class ResourceHandler implements HttpHandler {
@@ -20,26 +21,45 @@ public class ResourceHandler implements HttpHandler {
 
         ClassLoader classLoader = ResourceHandler.class.getClassLoader();
 
-        InputStream inputStream = classLoader.getResourceAsStream(STATIC_PATH + filePath);
 
-        if (inputStream == null) {
-            throw new IllegalArgumentException("File not found! : static" + filePath);
-        }
-
-        try (inputStream) {
+        // 자원 스트림을 가져오고 읽어서 body에 쓰기
+        try (InputStream inputStream = getResourceStream(classLoader, filePath)) {
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
                 response.getBody().write(buffer, 0, bytesRead);
             }
-        } catch (IllegalArgumentException e) {
-            log.error("Failed to read file", e);
+        } catch (IOException e) {
+            log.error("리소스 파일을 읽는 중 오류 발생: {}", filePath, e);
             throw e;
         }
 
+        // MIME 타입 설정
         Mime mime = Mime.ofFilePath(filePath);
         response.getHttpHeaders()
                 .addHeader(HeaderConstants.CONTENT_TYPE, mime.getType());
+    }
 
+    private InputStream getResourceStream(final ClassLoader classLoader, String filePath) {
+        final String pathPart = filePath.substring(filePath.lastIndexOf('/') + 1);
+
+        if (pathPart.contains(".")) {
+            // 마지막 슬래시 이후 부분에 확장자가 있는 경우 파일로 간주
+            InputStream resourceStream = classLoader.getResourceAsStream(STATIC_PATH + filePath);
+            if (resourceStream == null) {
+                throw new RuntimeException("파일을 찾을 수 없습니다.");
+            }
+            return resourceStream;
+        }
+        // 마지막 슬래시 이후 부분에 확장자가 없는 경우 디렉터리로 간주하여 index.html 확인
+        if (!filePath.endsWith("/")) {
+            filePath += "/";
+        }
+        filePath += "index.html";
+        InputStream resourceStream = classLoader.getResourceAsStream(STATIC_PATH + filePath);
+        if (resourceStream == null) {
+            throw new RuntimeException("index.html 파일을 찾을 수 없습니다.");
+        }
+        return resourceStream;
     }
 }
