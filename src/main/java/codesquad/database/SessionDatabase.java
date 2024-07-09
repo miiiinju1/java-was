098eaebc11cf8abbbe8ef86c6eeb43bd36686e51
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-09 12:34:02 +0900

    feat: SessionDatabase의 key값을 UUID String으로 변경

--- src/main/java/codesquad/database/SessionDatabase.java
+++ src/main/java/codesquad/database/SessionDatabase.java
@@ -8,23 +8,23 @@ public class SessionDatabase {
 
     // TODO 이후에 Long이 아니라 Session 객체를 저장하도록 변경하고, Session 객체에는 timeout 시간을 저장하도록 변경해야 함
     // TODO 지금 당장은 PK만 저장하도록
-    private static final Map<UUID, Long> registry = new ConcurrentHashMap<>();
+    private static final Map<String, Long> registry = new ConcurrentHashMap<>();
 
     public static String save(Long value) {
-        UUID key = UUID.randomUUID();
+        String key = UUID.randomUUID().toString();
         registry.put(key, value);
-        return key.toString();
+        return key;
     }
 
-    public static Long find(UUID key) {
+    public static Long find(String key) {
         return registry.get(key);
     }
 
-    public static void delete(UUID key) {
+    public static void delete(String key) {
         registry.remove(key);
     }
 
-    public static boolean containsKey(UUID key) {
+    public static boolean containsKey(String key) {
         return registry.containsKey(key);
     }
 
