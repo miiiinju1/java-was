de5f02c58dc71cd17562ac459ef28b63fc9df093
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-09 14:32:23 +0900

    chore: TODO 주석 제거

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -42,9 +42,6 @@ public class Main {
         handlerRegistry.registerHandler(HttpMethod.POST, "/users/create", registerUserHandler, registerUserLogic);
 
         // 로그인 로직
-        // TODO 로그인 실패 시 예외 전파하여 failed index.html 찾게 변경하기
-        // 쿠키 발급 구현
-        // ApiRequestHandlerAdapter를 공통적으로 구현하고, 추가적으로 setStatus나 redirectUrl은 추가 메서드를 통해 설정하도록 변경하기
         LoginUserLogic loginUserLogic = new LoginUserLogic(userDatabase);
         ArgumentResolver<LoginRequest> loginArgumentResolver = new LoginArgumentResolver();
         LoginRequestHandlerAdapter loginUserHandler = new LoginRequestHandlerAdapter(loginArgumentResolver);
