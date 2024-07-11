704d3b87721e8ba1b79072e74a1c61f36e5494ff
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-11 12:59:09 +0900

    chore: 동기화 문제 해결을 위한 주석 추가

--- src/main/java/codesquad/database/UserDatabase.java
+++ src/main/java/codesquad/database/UserDatabase.java
@@ -6,17 +6,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class UserDatabase extends Database<User> {
 
-    private final ConcurrentHashMap<String, Object> unqiue = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Object> unique = new ConcurrentHashMap<>();
     private static final Object PRESENT = new Object();
 
     @Override
     public long save(User user) {
 
         // 사용자 ID unique의 동기화 문제 해결을 위해 ConcurrentHashMap의 putIfAbsent 사용
-        Object value = unqiue.putIfAbsent(user.getUserId(), PRESENT);
+
+        // 적절한 방식
+        Object value = unique.putIfAbsent(user.getUserId(), PRESENT);
         if (value != null) {
             throw new IllegalArgumentException("이미 존재하는 userId 입니다.");
         }
+
+        // 잘못된 방식
+//        if(unique.containsKey(user.getUserId())) {
+//            throw new IllegalArgumentException("이미 존재하는 userId 입니다.");
+//        }
+//        unique.put(user.getUserId(), PRESENT);
+        //////////////////////////////////////////////////////
         return super.save(user);
     }
 
