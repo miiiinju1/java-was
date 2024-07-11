dea5c8754a97113f32966bd1f815e6ef84c5c272
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-11 12:58:46 +0900

    docs: 동기화 문제를 확인하기 위한 테스트메서드 추가

--- src/test/java/codesquad/database/UserDatabaseTest.java
+++ src/test/java/codesquad/database/UserDatabaseTest.java
@@ -1,11 +1,15 @@
 package codesquad.database;
 
+import codesquad.model.User;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserDatabaseTest {
 
@@ -42,6 +46,44 @@ class UserDatabaseTest {
 
         // then
         assertThat(s2).isEqualTo(a_);
+    }
+
+
+    @Test
+    @Disabled("동기화 문제를 확인하기 위한 테스트")
+    void testConcurrentDuplicateUserId() throws InterruptedException {
+        final int THREAD_COUNT = 10;
+        UserDatabase userDatabase = new UserDatabase();
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+        CyclicBarrier barrier = new CyclicBarrier(THREAD_COUNT);
+        AtomicInteger duplicateCount = new AtomicInteger(0);
+        String duplicateUserId = "duplicateUser";
+
+        int loopCount = THREAD_COUNT * 10;
+
+        for (int i = 0; i < loopCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    barrier.await();
+                    User user = new User(duplicateUserId, "password", "name", "email");
+                    userDatabase.save(user);
+
+                } catch (IllegalArgumentException e) {
+                    duplicateCount.incrementAndGet();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+        // 중복된 userId로 저장된 사용자 수는 1이어야 함
+        assertEquals(1, userDatabase.findAll().size());
+
+        // 중복된 userId로 저장된 사용자 수는 loopCount - 1이어야 함
+        assertEquals(loopCount - 1, duplicateCount.get());
     }
 
 }
