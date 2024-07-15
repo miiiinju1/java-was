5d5fdce15d6bfa84c34e3d9711858f221dcd3428
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-15 23:12:02 +0900

    refactor: Post에서 userId부분이 빠져있어서 추가함

--- src/main/java/codesquad/application/model/post/Post.java
+++ src/main/java/codesquad/application/model/post/Post.java
@@ -3,11 +3,15 @@ package codesquad.application.model.post;
 public class Post {
 
     private Long postId;
+    private final Long userId;
     private final Content content;
     private final ImagePath imagePath;
 
     public Long getPostId() {
         return postId;
+    }
+    public Long getUserId() {
+        return userId;
     }
 
     public Content getContent() {
@@ -23,9 +27,11 @@ public class Post {
     }
 
     public Post(
+            Long userId,
             String content,
             String imagePath
     ) {
+        this.userId = userId;
         this.content = new Content(content);
         this.imagePath = new ImagePath(imagePath);
     }
