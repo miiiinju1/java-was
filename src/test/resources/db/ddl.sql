dbfdd10d7ff191363badf500410f3bafef021379
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-15 20:44:22 +0900

    test: 테스트에 사용할 ddl 생성

---
+++ src/test/resources/db/ddl.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+       user_id INT PRIMARY KEY AUTO_INCREMENT,
+       username VARCHAR(50) NOT NULL UNIQUE,
+       password VARCHAR(255) NOT NULL,
+       email VARCHAR(100) NOT NULL UNIQUE,
+       name VARCHAR(100) NOT NULL,
+       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE posts (
+       post_id INT PRIMARY KEY AUTO_INCREMENT,
+       user_id INT NOT NULL,
+       title VARCHAR(255) NOT NULL,
+       content TEXT NOT NULL,
+       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+       FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+CREATE TABLE comments (
+      comment_id INT PRIMARY KEY AUTO_INCREMENT,
+      post_id INT NOT NULL,
+      user_id INT NOT NULL,
+      content TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (post_id) REFERENCES posts(post_id),
+      FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
