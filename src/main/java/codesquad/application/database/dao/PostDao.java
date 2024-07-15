f15c4c945612c8e2c3df2b5ddb0ff7c2f7f9e8b9
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-15 23:27:44 +0900

    feat: PostDao CRUD를 진행하는 PostDao와 PostDaoImpl 생성

---
+++ src/main/java/codesquad/application/database/dao/PostDao.java
@@ -0,0 +1,8 @@
+package codesquad.application.database.dao;
+
+import codesquad.application.database.Database;
+import codesquad.application.repository.vo.PostVO;
+
+public interface PostDao extends Database<PostVO> {
+
+}
