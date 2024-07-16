a1e637f6edea88006169ce54bc5cb64de1b629e1
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-16 10:29:18 +0900

    feat: Comment를 읽어올 때 사용할 CommentDao 구현

---
+++ src/main/java/codesquad/application/database/dao/CommentDao.java
@@ -0,0 +1,7 @@
+package codesquad.application.database.dao;
+
+import codesquad.application.database.Database;
+import codesquad.application.repository.vo.CommentVO;
+
+public interface CommentDao extends Database<CommentVO> {
+}
