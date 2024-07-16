88e723b77b1afaea14e4e26cff759efb1fa6e662
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-16 14:39:41 +0900

    feat: main의 ddl에 기초 데이터 insert 로직 추가

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
                        user_id INT PRIMARY KEY AUTO_INCREMENT,
                        username VARCHAR(50) NOT NULL UNIQUE,
                        password VARCHAR(255) NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE users (
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS POSTS (
                        post_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                        user_id BIGINT NOT NULL,
                        content VARCHAR(255) NOT NULL,
@@ -15,10 +15,21 @@ CREATE TABLE posts (
 );
 
 
-CREATE TABLE comments (
+CREATE TABLE IF NOT EXISTS comments (
                           comment_id INT PRIMARY KEY AUTO_INCREMENT,
                           post_id INT NOT NULL,
                           user_id INT NOT NULL,
                           content TEXT NOT NULL,
                           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO USERS (USER_ID, USERNAME, PASSWORD, EMAIL, NAME, CREATED_AT) VALUES
+    (1, 'd', 'd', 'd@n', 'd', '2024-07-16 14:35:37.823662');
+
+INSERT INTO POSTS (POST_ID, USER_ID, CONTENT, IMAGE_PATH) VALUES
+    (1, 1, 'asdf', '3479a814-0272-49d8-8699-e318f23cc902.png');
+
+INSERT INTO COMMENTS (COMMENT_ID, POST_ID, USER_ID, CONTENT, CREATED_AT) VALUES
+     (1, 1, 1001, 'This is the first comment.', '2024-07-16 14:30:00'),
+     (2, 1, 1002, 'This is the second comment.', '2024-07-16 14:31:00'),
+     (3, 1, 1003, 'This is the third comment.', '2024-07-16 14:32:00');
