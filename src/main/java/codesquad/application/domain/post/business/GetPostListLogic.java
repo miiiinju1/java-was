f91e17886a60533df06b4c1d4eb625e0dfda9bc5
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-16 16:55:51 +0900

    feat: post를 가져올 때 findAllJoinFetch를 사용하여 사용자 닉네임까지 함께 반환하도록 수정

--- src/main/java/codesquad/application/domain/post/business/GetPostListLogic.java
+++ src/main/java/codesquad/application/domain/post/business/GetPostListLogic.java
@@ -21,21 +21,37 @@ public class GetPostListLogic implements Triggerable<Void, PostListResponse> {
 
     @Override
     public PostListResponse run(Void unused) {
-        // TODO nickname을 다른 걸로 변경하기
+//        // TODO nickname을 다른 걸로 변경하기
+//
+//        // TODO 현재 단순 구현 용으로 반복 내에서 comment를 찾는 로직이 들어가 있음, connection도 많이 소비하게 될 테니, 한 connection을 사용할 수 있도록 만들어야하고,
+//        // TODO 해당 부분은 bulk로 처리할 수 있도록 만들어야 함
+//        List<PostResponse> postResponseList = postDao.findAll().stream()
+//                .map(post -> {
+//                    List<CommentVO> commentsOfPost = commentDao.findByPostId(post.postId());
+//
+//                    List<CommentResponse> commentResponse = commentsOfPost.stream()
+//                            .map(comment -> new CommentResponse(comment.commentId(), "commentNickname", comment.content()))
+//                            .toList();
+//                    return new PostResponse(post.postId(), "nickname", post.content(), post.imagePath(), CommentListResponse.of(post.postId(), commentResponse));
+//                })
+//                .collect(Collectors.toCollection(ArrayList::new));
+//        Collections.reverse(postResponseList);
+//        return PostListResponse.of(postResponseList);
 
-        // TODO 현재 단순 구현 용으로 반복 내에서 comment를 찾는 로직이 들어가 있음, connection도 많이 소비하게 될 테니, 한 connection을 사용할 수 있도록 만들어야하고,
-        // TODO 해당 부분은 bulk로 처리할 수 있도록 만들어야 함
-        List<PostResponse> postResponseList = postDao.findAll().stream()
+        // TODO 혹시 모르니깐 기존 로직은 살려두고 join을 쓰는 방법으로 변경
+        List<PostResponse> postResponseList = postDao.findAllJoinFetch().stream()
                 .map(post -> {
                     List<CommentVO> commentsOfPost = commentDao.findByPostId(post.postId());
+
                     List<CommentResponse> commentResponse = commentsOfPost.stream()
                             .map(comment -> new CommentResponse(comment.commentId(), "commentNickname", comment.content()))
                             .toList();
-                    return new PostResponse(post.postId(), "nickname", post.content(), post.imagePath(), CommentListResponse.of(post.postId(), commentResponse));
+                    return new PostResponse(post.postId(), post.nickname(), post.content(), post.imagePath(), CommentListResponse.of(post.postId(), commentResponse));
                 })
                 .collect(Collectors.toCollection(ArrayList::new));
         Collections.reverse(postResponseList);
         return PostListResponse.of(postResponseList);
+
     }
 
     public GetPostListLogic(
