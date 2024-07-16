8870839240b5c89918e480f0f3f73356391b1e07
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-16 10:26:00 +0900

    feat: main ddl의 post에 createdAt 필드 추가

--- src/test/resources/db/ddl.sql
+++ src/test/resources/db/ddl.sql
@@ -12,7 +12,8 @@ CREATE TABLE posts (
        post_id BIGINT PRIMARY KEY AUTO_INCREMENT,
        user_id BIGINT NOT NULL,
        content VARCHAR(255) NOT NULL,
-       image_path VARCHAR(255) NOT NULL
+       image_path VARCHAR(255) NOT NULL,
+       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
