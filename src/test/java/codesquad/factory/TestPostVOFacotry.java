d520ad70779135015ca738339f83198abeb0e90a
miiiinju1 <gms07073@ynu.ac.kr>
2024-07-17 12:28:09 +0900

    test: 테스트에 사용할 PostVO Factory와 CommentVO Factory 구현

---
+++ src/test/java/codesquad/factory/TestPostVOFacotry.java
@@ -0,0 +1,28 @@
+package codesquad.factory;
+
+import codesquad.application.database.vo.PostVO;
+
+public class TestPostVOFacotry {
+
+        public static PostVO createDefaultPostVO(Long userId) {
+            return new PostVO(
+                    null,
+                    userId,
+                    "content",
+                    "imagePath"
+            );
+        }
+
+        public static PostVO createBy(
+                Long userId,
+                String content,
+                String imageName
+        ) {
+            return new PostVO(
+                    null,
+                    userId,
+                    content,
+                    imageName
+            );
+        }
+}
